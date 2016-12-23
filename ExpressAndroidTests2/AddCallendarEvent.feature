89cc2b83c8bc9864737de04253669b1392e102d6
Adrian Szymański <aszymanski@infusion.com>
2016-12-23 09:39:18 +0100

    Add project files.

---
+++ ExpressAndroidTests2/AddCallendarEvent.feature
@@ -0,0 +1,24 @@
+﻿Feature: General Smoke Test
+
+	Scenario: Tap Add Callendar
+		Given I can see "Add calendar event" button
+		When  I tap "Add calendar event" buttoon
+		Then I can see "Add calendar event" button
+
+	Scenario: Tap Go Online Forms 1
+		Given I can see "Go online forms 1" button
+		When I tap "Go online forms 1" buttoon
+		Then I can see "Online Form" screen
+
+	Scenario: Tap Go Online Forms 2
+		Given I can see "Go online forms 2" button
+		When I tap "Go online forms 2" buttoon
+		Then I can see "Online Form" screen
+
+	Scenario: Show PDF
+		Given I can see "Show PDF" button
+		When  I tap "Show PDF" buttoon
+		Then I can see "PDFView" screen
+
+	#Scenario: Launch Repl app
+	#	Given Launch Repl
---
+++ ExpressAndroidTests2/AddCallendarEventSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace ExpressAndroidTests2
+{
+    [Binding]
+    public class AddCallendarEventSteps
+    {
+        AndroidApp app;
+        
+        [BeforeScenario]
+        public void SetUp()
+        {
+            app = ConfigureApp.Android.ApkFile("D:/express.droid (2).apk").StartApp();
+        }
+
+        [Given(@"Launch Repl")]
+        public void LaunchRepl()
+        {
+            app.Repl();
+
+        }
+
+        [Given(@"I can see ""(.*)"" button")]
+        public void GivenICanSeeButton(string buttonName)
+        {
+            app.WaitForElement(c => c.Marked(buttonName));
+
+        }
+        
+        [When(@"I tap ""(.*)"" buttoon")]
+        public void WhenITapButtoon(string buttonName)
+        {
+            app.Tap(c => c.Class("Button").Text(buttonName));
+        }
+        
+        [Then(@"I can see ""(.*)"" button")]
+        public void ThenICanSeeButton(string buttonName)
+        {
+            app.WaitForElement(c => c.Marked(buttonName));
+        }
+
+        [Then(@"I can see ""(.*)"" screen")]
+        public void ThenICanSeeScreen(string screenName)
+        {
+            app.WaitForElement(c => c.Marked("action_bar_title").Text(screenName));
+            //app.Repl();
+            //app.WaitForElement(c => c.Marked(buttonName));
+        }
+    }
+}
