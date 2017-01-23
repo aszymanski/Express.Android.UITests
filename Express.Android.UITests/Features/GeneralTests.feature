abbbc2eb94cc2f3fb0256211297bcfcd7e1616d7
Adrian Szymański <aszymanski@infusion.com>
2017-01-23 18:20:58 +0100

    Renamed project

---
+++ Express.Android.UITests/Features/GeneralTests.feature
@@ -0,0 +1,39 @@
+﻿Feature: General Smoke Test
+	Scenario: Tap Add Callendar
+		Given I can see "Add calendar event" button
+		When  I tap "Add calendar event" button
+		Then I can see "Add calendar event" button
+
+	Scenario: Tap Go Online Forms 1
+		Given I can see "Go online forms 1" button
+		When I tap "Go online forms 1" button
+		Then I can see "Online Form" screen
+		When I fill the Online Form 1
+		And I tap "Submit" button
+		Then I can see "Results" screen
+		When I tap "Sign it!" button
+		Then I can see "Signature" screen
+		When I perform a signature
+		And I tap "Results" button
+		Then I can see "Results" screen
+		#When I scroll down to Sign In button
+		#And I tap "Sign it!" button
+
+	Scenario: Tap Go Online Forms 2
+		Given I can see "Go online forms 2" button
+		When I tap "Go online forms 2" button
+		Then I can see "Online Form" screen
+
+	Scenario: Show PDF
+		Given I can see "Show PDF" button
+		When  I tap "Show PDF" button
+		Then Popup appears "Do you want to open in external viewer?"
+
+		When I tap "Cancel" button
+		Then I can see "PDFView" screen
+
+	#Scenario: Launch Repl app
+	#	Given Launch Repl
+
+	Scenario: Sample Page Object test
+		Given Sample "Go online forms 1" press
---
+++ Express.Android.UITests/StepDefinitions/GeneralTestStepsSteps.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+using System.Linq;
+using ExpressAndroidTests2.Pages;
+using NUnit.Framework;
+
+namespace ExpressAndroidTests2
+{
+    [Binding]
+    public class GeneralTestStepsSteps
+    {
+
+        AndroidApp app;
+        
+        [BeforeScenario]
+        public void SetUp()
+        {
+            app = ConfigureApp.Android.EnableLocalScreenshots().ApkFile("D:/express.droid.apk").StartApp();
+            //app.Device.SetLocation(40.71448, -74.00598);
+
+        }
+
+        [Given(@"Launch Repl")]
+        public void LaunchRepl()
+        {
+            app.Repl();
+        }
+
+        [Given(@"I can see ""(.*)"" button")]
+        public void GivenICanSeeButton(string buttonName)
+        {
+            AbstractPage page = new AbstractPage(app);
+            bool assertion = page.CheckIfButtonIsDisplayed(buttonName);
+            app.Screenshot("Button check - " + buttonName);
+            Assert.IsTrue(assertion);
+        }    
+
+        [When(@"I fill the Online Form 1")]
+        public void WhenIFillOnlineForm1()
+        {
+            ScrollStrategy strategy = ScrollStrategy.Auto;
+            app.WaitForElement(c => c.Class("TextView").Text("What filing status will you use on your 2016 Income Tax Return?"));
+            app.Tap(c => c.Class("Textview").Text("Single"));
+            app.ScrollDownTo(c => c.Class("TextView").Text("Can someone else claim you as a dependent on his or her tax return?"));
+            app.Tap(c => c.Class("ToggleButton").Text("Yes"));
+            app.ScrollDownTo(c => c.Class("TextView").Text("How many dependents (other than yourself and your spouse ) you will claim on your tax return?"));
+            app.ClearText(c => c.Class("EditText"));
+            app.EnterText(c => c.Class("EditText"),"1");
+            app.Back();
+            app.ScrollDownTo(c => c.Class("TextView").Text("Are you going to file as head of household?"));
+            app.Tap(c => c.Class("ToggleButton").Text("Yes").Index(0));
+            app.Tap(c => c.Class("ToggleButton").Text("Yes").Index(1));
+
+            app.ScrollDownTo(c => c.Id("month"));
+            AppResult[] result = app.Query(c => c.Id("numberpicker_input").Text("Jun"));
+
+            while (result.Any() == false)
+            {
+                Console.WriteLine("Rezultaty" + result.Any());
+                app.ScrollDown((m => m.Id("month")), strategy, 0.30, 900);
+                app.Tap(m => m.Id("month"));
+                result = app.Query(c => c.Id("numberpicker_input").Text("Jun"));
+
+            }
+            app.EnterText(c => c.Id("year"), "2001");
+            app.EnterText(c => c.Id("day"), "11");
+            app.PressUserAction();
+        }
+
+         [When(@"I scroll down to Sign In button")]
+        public void ScrollToSignItButton()
+        {
+            app.ScrollDownTo(c => c.Class("Button").Text("Sign it!"));
+        }
+
+    
+        [When(@"I tap ""(.*)"" button")]
+        public void WhenITapButtoon(string buttonName)
+        {
+            app.Tap(c => c.Class("Button").Text(buttonName));
+        }
+
+        [When(@"I perform a signature")]
+        public void WhenIPerformASignature()
+        {
+            ScrollStrategy strategy = ScrollStrategy.Auto;
+            app.ScrollDown((m => m.Id("signatureView")), strategy, 0.30, 900);
+        }
+
+        [Then(@"I can see ""(.*)"" button")]
+        public void ThenICanSeeButton(string buttonName)
+        {
+            app.WaitForElement(c => c.Marked(buttonName));
+        }
+
+        [Then(@"Popup appears ""(.*)""")]
+        public void ThenPopupAppear(string msg)
+        {
+            app.WaitForElement(c => c.Class("TextView").Text(msg));
+
+        }
+
+        [Then(@"I can see ""(.*)"" screen")]
+        public void ThenICanSeeScreen(string screenName)
+        {
+            app.WaitForElement(c => c.Id("action_bar_title").Text(screenName));
+
+        }
+
+        [Given(@"Sample ""(.*)"" press")]
+        public void ThSamplePageObject(String buttonName)
+        {
+            Dashboard dashboard = new Dashboard(app);
+
+            dashboard.CheckIfButtonIsDisplayed(buttonName);
+            dashboard.PressButton(buttonName);
+            Form1 form1 = new Form1(app);
+            app.WaitForElement(form1.QuestionHeader("What filing status will you use on your 2016 Income Tax Return?"));
+            app.Screenshot("Button check - " + buttonName);
+        }
+    }
+}
